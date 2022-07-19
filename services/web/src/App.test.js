a843aa81b989a69f965d68a6b301e7195c363edb
Muhammad Hassan Shakeel <mhassan20896@gmail.com>
2022-07-19 22:56:24 +0500

    Update App.test.js

--- services/web/src/App.test.js
+++ services/web/src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Main from './views/main/main';
 
+
+
 test('renders learn react link', () => {
   const { getByText } = render(<Main />);
   const linkElement = getByText(/learn react/i);
